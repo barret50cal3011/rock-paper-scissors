5e5027378f307d3e5af8b159a2a10d92871689be
B4RRET <barret50cal3011@gmail.com>
2022-10-04 16:46:59 -0500

    created Player and Hand class, added all the html components to the js, create player objects

--- js/elder_brain.js
+++ js/elder_brain.js
@@ -1,2 +1,59 @@
+const start_button = document.getElementById("start-button");
+const select_button = document.getElementById("select-button");
 
-//alert("js works");
+const input_player1 = document.getElementById("player1-name");
+const input_player2 = document.getElementById("player2-name");
+
+const option_section = document.getElementById("options");
+const results = document.getElementById("result");
+
+const player1_info = document.getElementById("player1");
+const player2_info = document.getElementById("player2");
+
+const player1_name = document.getElementById("name1");
+const player2_name = document.getElementById("name2");
+const player1_hp = document.getElementById("hp1");
+const player2_hp = document.getElementById("hp2");
+class Hand{
+    constructor(name){
+        this.name = name;
+        this.wins = [];
+    }
+}
+class Player{
+    constructor(name, hp){
+        this.name = name;
+        this.hp = hp;
+    }
+}
+
+let hands = [];
+let player1 = new Player("", 0);
+let player2 = new Player("", 0);
+
+function init_game(){
+    start_button.addEventListener("click", start_battle);
+    select_button.addEventListener("click", seclect_hand);
+}
+
+function start_battle(){
+    let name1 = input_player1.value;
+    let name2 = input_player2.value;
+
+    if(name1 == ""){name1 = "Player";}
+    if(name2 == ""){name2 = "PC";}
+
+    player1 = new Player(name1, 5);
+    player2 = new Player(name2, 5);
+
+    player1_name.innerHTML = player1.name;
+    player2_name.innerHTML = player2.name;
+    player1_hp.innerHTML = player1.hp;
+    player2_hp.innerHTML = player2.hp;
+}
+
+function seclect_hand(){
+
+}
+
+init_game();
